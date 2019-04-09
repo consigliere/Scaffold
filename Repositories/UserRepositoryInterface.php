22fe38e21576a82ab1d81599a81141ac282f831f
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:41:43 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/UserRepositoryInterface

---
+++ Repositories/UserRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 12:02 AM
+ */
+
+/**
+ * UserRepositoryInterface.php
+ * Created by @anonymoussc on 04/08/2019 11:53 PM.
+ */
+
+namespace App\Components\Scaffold\Repositories;
+
+
+interface UserRepositoryInterface
+{
+
+}
