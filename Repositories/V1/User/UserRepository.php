fd46c9e1040757dea438140998190f1dfab9a3c7
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:31:13 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/V1/User/UserRepository

---
+++ Repositories/V1/User/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 10:37 AM
+ */
+
+/**
+ * UserRepository.php
+ * Created by @anonymoussc on 04/09/2019 8:35 AM.
+ */
+
+namespace App\Components\Scaffold\Repositories\V1\User;
+
+use App\Components\Scaffold\Repositories\User\UserRepository as UserBaseRepository;
+use App\Components\Scaffold\Repositories\V1\UserRepositoryInterface;
+
+class UserRepository extends UserBaseRepository implements UserRepositoryInterface
+{
+
+}
