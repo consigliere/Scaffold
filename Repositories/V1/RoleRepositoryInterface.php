f300769845fbb4e5a57c03ed47dcfbc9c6f69545
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:32:20 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/V1/RoleRepositoryInterface

---
+++ Repositories/V1/RoleRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 10:37 AM
+ */
+
+/**
+ * RoleRepositoryInterface.php
+ * Created by @anonymoussc on 04/09/2019 8:45 AM.
+ */
+
+namespace App\Components\Scaffold\Repositories\V1;
+
+use App\Components\Scaffold\Repositories\RoleRepositoryInterface as RoleBaseRepositoryInterface;
+
+interface RoleRepositoryInterface extends RoleBaseRepositoryInterface
+{
+
+}
