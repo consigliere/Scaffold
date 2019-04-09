9e1eb34301dd39ddffd7e2debbdaf5078dcde0bf
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:45:31 +0700

    feat(services): new App/Components/Scaffold/Services/RoleService

---
+++ Services/RoleService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/8/19 11:51 PM
+ */
+
+/**
+ * RoleService.php
+ * Created by @anonymoussc on 04/08/2019 11:44 PM.
+ */
+
+namespace App\Components\Scaffold\Services;
+
+
+class RoleService extends Service
+{
+
+}
