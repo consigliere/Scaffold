518315a5dc195a6eb3cb5b8e3a2e8ca935fb54f6
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:05:35 +0700

    feat(entities): new App/Components/Scaffold/Entities/Permission

---
+++ Entities/Permission.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 12:59 PM
+ */
+
+/**
+ * Permission.php
+ * Created by @anonymoussc on 04/09/2019 12:58 PM.
+ */
+
+namespace App\Components\Scaffold\Entities;
+
+use TCG\Voyager\Models\Permission as AppPermission;
+
+class Permission extends AppPermission
+{
+
+}
