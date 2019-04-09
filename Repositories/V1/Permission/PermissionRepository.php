eb8f1eaaace421c9d00ec4951b2094bec1fa184c
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:29:53 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/V1/Permission/PermissionRepository

---
+++ Repositories/V1/Permission/PermissionRepository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 10:37 AM
+ */
+
+/**
+ * PermissionRepository.php
+ * Created by @anonymoussc on 04/09/2019 8:34 AM.
+ */
+
+namespace App\Components\Scaffold\Repositories\V1\Permission;
+
+use App\Components\Scaffold\Repositories\Permission\PermissionRepository as PermissionBaseRepository;
+use App\Components\Scaffold\Repositories\V1\PermissionRepositoryInterface;
+
+class PermissionRepository extends PermissionBaseRepository implements PermissionRepositoryInterface
+{
+
+}
