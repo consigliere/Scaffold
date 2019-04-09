9b6e6f96971a47cd89586915b5ec6676ed5f5b37
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:28:08 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/Permission/PermissionRepository

---
+++ Repositories/Permission/PermissionRepository.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 1:28 PM
+ */
+
+/**
+ * PermissionRepository.php
+ * Created by @anonymoussc on 04/09/2019 8:33 AM.
+ */
+
+namespace App\Components\Scaffold\Repositories\Permission;
+
+use App\Components\Scaffold\Repositories\PermissionRepositoryInterface;
+use App\Components\Scaffold\Repositories\Repository;
+
+class PermissionRepository extends Repository implements PermissionRepositoryInterface
+{
+
+    /**
+     * @return mixed
+     */
+    protected function getModel()
+    {
+        // TODO: Implement getModel() method.
+    }
+}
