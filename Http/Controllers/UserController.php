87483e97a9358324f5076626bca1dd18d47b53a8
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:21:42 +0700

    feat(controller): new App/Components/Scaffold/Http/Controllers/UserController

---
+++ Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 1:21 PM
+ */
+
+namespace App\Components\Scaffold\Http\Controllers;
+
+class UserController extends \App\Components\Scaffold\Http\Controllers\Controller
+{
+
+}
