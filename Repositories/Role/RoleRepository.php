a4a5467a19279c2a661f08d11f9e9131ce390b27
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:28:45 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/Role/RoleRepository

---
+++ Repositories/Role/RoleRepository.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 10:37 AM
+ */
+
+/**
+ * RoleRepository.php
+ * Created by @anonymoussc on 04/09/2019 8:32 AM.
+ */
+
+namespace App\Components\Scaffold\Repositories\Role;
+
+use App\Components\Scaffold\Repositories\Repository;
+use App\Components\Scaffold\Repositories\RoleRepositoryInterface;
+
+class RoleRepository extends Repository implements RoleRepositoryInterface
+{
+
+    /**
+     * @return mixed
+     */
+    protected function getModel()
+    {
+        // TODO: Implement getModel() method.
+    }
+}
