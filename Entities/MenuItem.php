de6ff21abe4c818772d7eb18714a59d8be2febb8
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:04:12 +0700

    feat(entities): new App/Components/Scaffold/Entities/MenuItem

---
+++ Entities/MenuItem.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 12:59 PM
+ */
+
+/**
+ * MenuItem.php
+ * Created by @anonymoussc on 04/09/2019 12:57 PM.
+ */
+
+namespace App\Components\Scaffold\Entities;
+
+use TCG\Voyager\Models\MenuItem as AppMenuItem;
+
+class MenuItem extends AppMenuItem
+{
+
+}
