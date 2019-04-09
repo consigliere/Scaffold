4500e8f332042b821abe0aa4512d5e98a1dfcb22
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:21:14 +0700

    feat(controller): new App/Components/Scaffold/Http/Controllers/RoleController

---
+++ Http/Controllers/RoleController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 1:20 PM
+ */
+
+/**
+ * RoleController.php
+ * Created by @anonymoussc on 04/08/2019 11:36 PM.
+ */
+
+namespace App\Components\Scaffold\Http\Controllers;
+
+class RoleController extends Controller
+{
+
+}
