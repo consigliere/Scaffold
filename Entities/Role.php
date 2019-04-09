44ab5398f4eb06fdc20b7da3affa8c8c191471ca
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:07:51 +0700

    feat(entities): new App/Components/Scaffold/Entities/Role

---
+++ Entities/Role.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 12:59 PM
+ */
+
+/**
+ * Role.php
+ * Created by @anonymoussc on 04/09/2019 12:58 PM.
+ */
+
+namespace App\Components\Scaffold\Entities;
+
+use TCG\Voyager\Models\Role as AppRole;
+
+class Role extends AppRole
+{
+
+}
