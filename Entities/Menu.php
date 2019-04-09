19129bc0600efae7f9f991e6b654061be08310c1
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:03:29 +0700

    feat(entities): new App/Components/Scaffold/Entities/Menu

---
+++ Entities/Menu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 1:03 PM
+ */
+
+/**
+ * Menu.php
+ * Created by @anonymoussc on 04/09/2019 12:57 PM.
+ */
+
+namespace App\Components\Scaffold\Entities;
+
+use TCG\Voyager\Models\Menu as AppMenu;
+
+class Menu extends AppMenu
+{
+
+}
