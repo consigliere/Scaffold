515c29bc3e48b8e087dbd0d7afc2195c83e45c55
anonymoussc <50c5ac69@opayq.com>
2019-04-09 12:53:29 +0700

    feat(entities): new App/Components/Scaffold/Entities/Category

---
+++ Entities/Category.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/8/19 6:48 PM
+ */
+
+namespace App\Components\Scaffold\Entities;
+
+use TCG\Voyager\Models\Category as AppCategory;
+
+class Category extends AppCategory
+{
+
+}
