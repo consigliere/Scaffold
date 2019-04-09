1f67658d67226e99a79db2dd841b451f761a25f8
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:40:09 +0700

    feat(repositories): new App/Components/Scaffold/Repositories/PermissionRepositoryInterface

---
+++ Repositories/PermissionRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 12:02 AM
+ */
+
+/**
+ * PermissionRepositoryInterface.php
+ * Created by @anonymoussc on 04/08/2019 11:54 PM.
+ */
+
+namespace App\Components\Scaffold\Repositories;
+
+
+interface PermissionRepositoryInterface
+{
+
+}
