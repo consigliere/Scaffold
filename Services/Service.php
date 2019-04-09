618375aa129aa4a0a041e475d94ac6d4d6728cc6
anonymoussc <50c5ac69@opayq.com>
2019-04-09 13:46:03 +0700

    feat(services): new App/Components/Scaffold/Services/Service

---
+++ Services/Service.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 4/9/19 1:46 PM
+ */
+
+/**
+ * Service.php
+ * Created by @anonymoussc on 01/03/2019 5:55 PM.
+ */
+
+namespace App\Components\Scaffold\Services;
+
+use App\Components\Signal\Shared\ErrorLog;
+use App\Components\Signal\Shared\Signal;
+
+/**
+ * Class Service
+ * @package App\Components\Passerby\Services
+ */
+class Service
+{
+    use Signal, ErrorLog;
+
+}
