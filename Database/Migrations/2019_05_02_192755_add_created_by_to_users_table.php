e2148de264127a3058df05483fd996569f5d7e80
anonymoussc <50c5ac69@opayq.com>
2019-05-03 02:53:45 +0700

    feat(migrations): new ./Database/Migrations/2019_05_02_192755_add_created_by_to_users_table

---
+++ Database/Migrations/2019_05_02_192755_add_created_by_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/3/19 2:53 AM
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCreatedByToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->integer('created_by')->after('settings')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('created_by');
+        });
+    }
+}
