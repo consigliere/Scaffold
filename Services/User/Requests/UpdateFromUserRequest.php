7fb0490bb130b5b236a29cc02dc650b40e1f8caa
anonymoussc <50c5ac69@opayq.com>
2019-05-04 01:44:20 +0700

    refactor(users): modified ./Services/User/Requests/UpdateFromUserRequest

--- Services/User/Requests/UpdateFromUserRequest.php
+++ Services/User/Requests/UpdateFromUserRequest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/3/19 2:40 PM
+ * Last modified 5/4/19 1:23 AM
  */
 
 /**
@@ -15,24 +15,25 @@ class UpdateFromUserRequest
 {
     public function __invoke($uuid, array $data = [], array $option = [], array $param = [])
     {
+        $dataIn = $data['input'];
         $user   = [];
         $inList = [1, 2];
-        $roleId = (int)$data['roleId'];
+        $roleId = (int)$dataIn['roleId'];
 
-        if (!empty($data)) {
+        if (!empty($dataIn)) {
             $user = [
                 'role_id'           => in_array($roleId, $inList, true) ? $roleId : 2,
-                'username'          => strtolower($data['username']),
-                'name'              => $data['name'],
-                'email'             => strtolower($data['email']),
-                'avatar'            => $data['avatar'],
-                'email_verified_at' => $data['emailVerifiedAt'],
-                'remember_token'    => $data['rememberToken'],
-                'settings'          => $data['settings'],
+                'username'          => strtolower($dataIn['username']),
+                'name'              => $dataIn['name'],
+                'email'             => strtolower($dataIn['email']),
+                'avatar'            => $dataIn['avatar'],
+                'email_verified_at' => $dataIn['emailVerifiedAt'],
+                'remember_token'    => $dataIn['rememberToken'],
+                'settings'          => $dataIn['settings'],
             ];
 
-            if (isset($data['password']) && !empty($data['password']) && ($data['password'] !== null)) {
-                $user['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+            if (isset($dataIn['password']) && !empty($dataIn['password']) && ($dataIn['password'] !== null)) {
+                $user['password'] = password_hash($dataIn['password'], PASSWORD_BCRYPT);
             }
         }
 
