7770ba5329cfc5298fdba810de022ab82c96c3a6
anonymoussc <50c5ac69@opayq.com>
2019-05-09 19:37:04 +0700

    refactor(users): new ./Services/User/Requests/UpdateFromUserRequest

---
+++ Services/User/Requests/UpdateUser.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/9/19 7:28 PM
+ */
+
+/**
+ * UpdateUser.php
+ * Created by @anonymoussc on 05/09/2019 7:20 PM.
+ */
+
+namespace App\Components\Scaffold\Services\User\Requests;
+
+/**
+ * Class UpdateUser
+ * @package App\Components\Scaffold\Services\User\Requests
+ */
+class UpdateUser
+{
+    /**
+     * @param array $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return array
+     */
+    public function __invoke(array $data = [], array $option = [], array $param = [])
+    {
+        $dataIn = $data['form'];
+        $user   = [];
+        $inList = [1, 2];
+        $roleId = (int)$dataIn['roleId'];
+
+        if (!empty($dataIn)) {
+            $user = [
+                'role_id'           => in_array($roleId, $inList, true) ? $roleId : 2,
+                'username'          => strtolower($dataIn['username']),
+                'name'              => $dataIn['name'],
+                'email'             => strtolower($dataIn['email']),
+                'avatar'            => $dataIn['avatar'],
+                'email_verified_at' => $dataIn['emailVerifiedAt'],
+                'remember_token'    => $dataIn['rememberToken'],
+                'settings'          => $dataIn['settings'],
+            ];
+
+            if (isset($dataIn['password']) && !empty($dataIn['password']) && ($dataIn['password'] !== null)) {
+                $user['password'] = password_hash($dataIn['password'], PASSWORD_BCRYPT);
+            }
+        }
+
+        return $user;
+    }
+}
