10b68e19066bc49343a9993567ac5341b7aadc5f
anonymoussc <50c5ac69@opayq.com>
2019-05-10 04:38:50 +0700

    refactor(users): modified ./Services/User/Shared/UserCallable

--- Services/User/Shared/UserCallable.php
+++ Services/User/Shared/UserCallable.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/4/19 4:10 AM
+ * Last modified 5/10/19 4:38 AM
  */
 
 /**
@@ -17,84 +17,5 @@ namespace App\Components\Scaffold\Services\User\Shared;
  */
 trait UserCallable
 {
-    /**
-     * @param callable $response
-     * @param          $dataObj
-     * @param array    $option
-     * @param array    $param
-     *
-     * @return mixed
-     */
-    public function browseResponse(Callable $response, $dataObj, array $option = [], array $param = [])
-    {
-        return $response($dataObj, $option, $param);
-    }
 
-    /**
-     * @param callable $create
-     * @param array    $data
-     * @param array    $option
-     * @param array    $param
-     *
-     * @return array
-     */
-    public function createData(Callable $create, array $data = [], array $option = [], $param = []): array
-    {
-        return $create($data, $option, $param);
-    }
-
-    /**
-     * @param callable $response
-     * @param          $dataObj
-     * @param array    $option
-     * @param array    $param
-     *
-     * @return array
-     */
-    public function createResponse(Callable $response, $dataObj, array $option = [], array $param = []): array
-    {
-        return $response($dataObj, $option, $param);
-    }
-
-    /**
-     * @param callable $response
-     * @param          $uuid
-     * @param          $dataObj
-     * @param array    $option
-     * @param array    $param
-     *
-     * @return mixed
-     */
-    public function readResponse(Callable $response, $uuid, $dataObj, array $option = [], array $param = [])
-    {
-        return $response($uuid, $dataObj, $option, $param);
-    }
-
-    /**
-     * @param callable $update
-     * @param          $uuid
-     * @param array    $data
-     * @param array    $option
-     * @param array    $param
-     *
-     * @return mixed
-     */
-    public function updateData(Callable $update, $uuid, array $data = [], array $option = [], array $param = [])
-    {
-        return $update($uuid, $data, $option, $param);
-    }
-
-    /**
-     * @param callable $response
-     * @param          $uuid
-     * @param          $dataObj
-     * @param array    $option
-     * @param array    $param
-     *
-     * @return mixed
-     */
-    public function updateResponse(Callable $response, $uuid, $dataObj, array $option = [], array $param = [])
-    {
-        return $response($uuid, $dataObj, $option, $param);
-    }
 }
