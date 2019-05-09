65c8608d1955df29f6929a06b0727e3d15dace8e
anonymoussc <50c5ac69@opayq.com>
2019-05-09 19:41:14 +0700

    refactor(users): new ./Services/User/Responses/UpdateUserResponse

---
+++ Services/User/Responses/UserResource.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/9/19 7:28 PM
+ */
+
+/**
+ * UserResource.php
+ * Created by @anonymoussc on 05/09/2019 7:25 PM.
+ */
+
+namespace App\Components\Scaffold\Services\User\Responses;
+
+/**
+ * Class UserResource
+ * @package App\Components\Scaffold\Services\User\Responses
+ */
+class UserResource
+{
+    /**
+     * @param       $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return array
+     */
+    public function __invoke($data, array $option = [], array $param = [])
+    {
+        $user = [];
+
+        if (!empty($data)) {
+            $user['data'] = [
+                'type'       => $param['type'],
+                'id'         => $data->uuid,
+                'attributes' => [
+                    'username' => $data->username,
+                    'name'     => $data->name,
+                    'email'    => $data->email,
+                    'avatar'   => $data->avatar,
+                    'settings' => $data->settings,
+                ],
+            ];
+
+            if ($option['api.hasLink']) {
+                $user['link'] = [
+                    'self' => $param['link.fullUrl'],
+                ];
+            }
+
+            if ($option['api.hasMeta']) {
+                $user['meta'] = [
+                    'copyright' => 'copyrightⒸ ' . date('Y') . ' ' . $param['app.name'],
+                    'author'    => $param['api.meta.author'],
+                ];
+            }
+        }
+
+        return $user;
+    }
+}
