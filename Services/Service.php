728edbf09b6e36c380aee7dd03b6aa82c8161d97
anonymoussc <50c5ac69@opayq.com>
2019-05-09 19:43:37 +0700

    refactor(users): modified ./Services/Service

--- Services/Service.php
+++ Services/Service.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/3/19 2:49 AM
+ * Last modified 5/9/19 7:43 PM
  */
 
 /**
@@ -23,4 +23,30 @@ use App\Components\Signature\Services\SignatureService as BaseService;
 class Service extends BaseService
 {
     use Signal, ErrorLog;
+
+    /**
+     * @param callable $reform
+     * @param array    $data
+     * @param array    $option
+     * @param array    $param
+     *
+     * @return array
+     */
+    public function reform(Callable $reform, array $data = [], array $option = [], $param = []): array
+    {
+        return $reform($data, $option, $param);
+    }
+
+    /**
+     * @param callable $response
+     * @param          $dataObj
+     * @param array    $option
+     * @param array    $param
+     *
+     * @return array
+     */
+    public function transform(Callable $response, $dataObj, array $option = [], array $param = []): array
+    {
+        return $response($dataObj, $option, $param);
+    }
 }
