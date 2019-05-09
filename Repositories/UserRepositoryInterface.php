639cda76f3a9caba558f602383b35fcdfc856356
anonymoussc <50c5ac69@opayq.com>
2019-05-09 19:34:07 +0700

    refactor(users): modified ./Repositories/UserRepositoryInterface

--- Repositories/UserRepositoryInterface.php
+++ Repositories/UserRepositoryInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 4/29/19 3:15 AM
+ * Last modified 5/9/19 4:49 AM
  */
 
 /**
@@ -14,8 +14,9 @@ namespace App\Components\Scaffold\Repositories;
 
 interface UserRepositoryInterface
 {
+    public function browse(array $data = [], array $option = [], array $param = []);
+
     public function create(array $data = [], array $option = [], array $param = []);
 
     public function update($id, array $data = [], array $option = [], array $param = []);
-
 }
