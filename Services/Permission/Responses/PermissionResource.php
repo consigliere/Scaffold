9c427acd7b92e50a4e8521148a646dcfd88dc0e8
anonymoussc <50c5ac69@opayq.com>
2019-05-13 07:33:48 +0700

    feat(permissions): CRUD operations - new ./Services/Permission/Responses/PermissionResource

---
+++ Services/Permission/Responses/PermissionResource.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/13/19 7:33 AM
+ */
+
+/**
+ * PermissionResource.php
+ * Created by @anonymoussc on 05/12/2019 9:27 AM.
+ */
+
+namespace App\Components\Scaffold\Services\Permission\Responses;
+
+/**
+ * Class PermissionResource
+ * @package App\Components\Scaffold\Services\Permission\Responses
+ */
+class PermissionResource
+{
+    /**
+     * @param       $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return array
+     */
+    public function __invoke($data, array $option = [], array $param = [])
+    {
+        $user = [];
+
+        if (!empty($data)) {
+            $user['data'] = [
+                'type'       => $param['type'],
+                'id'         => $data->uuid,
+                'attributes' => [
+                    'key'    => $data->key,
+                    'entity' => $data->table_name,
+                ],
+            ];
+
+            if ($option['api.hasLink']) {
+                $user['link'] = [
+                    'self' => $param['link.fullUrl'],
+                ];
+            }
+
+            if ($option['api.hasMeta']) {
+                $user['meta'] = [
+                    'copyright' => 'copyrightⒸ ' . date('Y') . ' ' . $param['app.name'],
+                    'author'    => $param['api.meta.author'],
+                ];
+            }
+        }
+
+        return $user;
+    }
+}
