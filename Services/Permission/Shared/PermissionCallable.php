e877155c338a1851476e3024fde80c9fdec605e4
anonymoussc <50c5ac69@opayq.com>
2019-05-13 07:35:02 +0700

    feat(permissions): CRUD operations - new ./Services/Permission/Shared/PermissionCallable

---
+++ Services/Permission/Shared/PermissionCallable.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/12/19 9:37 AM
+ */
+
+/**
+ * PermissionCallable.php
+ * Created by @anonymoussc on 05/12/2019 9:31 AM.
+ */
+
+namespace App\Components\Scaffold\Services\Permission\Shared;
+
+/**
+ * Trait PermissionCallable
+ * @package App\Components\Scaffold\Services\Permission\Shared
+ */
+trait PermissionCallable
+{
+
+}
