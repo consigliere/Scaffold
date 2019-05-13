6ae237276e0b50d444b2c7d8f43a5fc32fcff65c
anonymoussc <50c5ac69@opayq.com>
2019-05-13 07:31:56 +0700

    feat(permissions): CRUD operations - new Services ./Services/Permission/Requests/UpdatePermission

---
+++ Services/Permission/Requests/UpdatePermission.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/13/19 7:31 AM
+ */
+
+/**
+ * UpdatePermission.php
+ * Created by @anonymoussc on 05/12/2019 9:26 AM.
+ */
+
+namespace App\Components\Scaffold\Services\Permission\Requests;
+
+/**
+ * Class UpdatePermission
+ * @package App\Components\Scaffold\Services\Permission\Requests
+ */
+class UpdatePermission
+{
+    /**
+     * @param array $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return array
+     */
+    public function __invoke(array $data = [], array $option = [], array $param = [])
+    {
+        $dataIn     = $data['form'];
+        $permission = [];
+
+        if (!empty($dataIn)) {
+            $permission = [
+                'key'        => $dataIn['key'],
+                'table_name' => $dataIn['entity'],
+            ];
+        }
+
+        return $permission;
+    }
+}
