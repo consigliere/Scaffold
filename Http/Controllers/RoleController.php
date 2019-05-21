a99b83d5120e974fe86ba1e205f867fac755c533
anonymoussc <50c5ac69@opayq.com>
2019-05-21 14:49:32 +0700

    refactor(ctrls): modified ./Http/Controllers/RoleController

--- Http/Controllers/RoleController.php
+++ Http/Controllers/RoleController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/19/19 1:52 PM
+ * Last modified 5/21/19 1:03 PM
  */
 
 /**
@@ -36,7 +36,7 @@ class RoleController extends Controller
     public function __construct(RoleService $RoleService)
     {
         $this->roleService = $RoleService;
-        $this->euuid       = $this->getUuid();
+        $this->euuid       = randomUuid();
         $this->type        = 'roles';
     }
 
