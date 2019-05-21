a89296d93edc15f12832d6cf1c0702a6b3dde016
anonymoussc <50c5ac69@opayq.com>
2019-05-22 01:49:10 +0700

    refactor(repositories): modified ./Repositories/Repository

--- Repositories/Repository.php
+++ Repositories/Repository.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 5/10/19 7:23 PM
+ * Last modified 5/22/19 1:48 AM
  */
 
 /**
@@ -11,9 +11,8 @@
 
 namespace App\Components\Scaffold\Repositories;
 
-use App\Components\Signal\Shared\{
-    ErrorLog, Signal
-};
+use App\Components\Signal\Shared\ErrorLog;
+use App\Components\Signal\Shared\Signal;
 use App\Components\Signature\Repositories\SignatureRepository as BaseRepository;
 
 /**
@@ -25,12 +24,42 @@ abstract class Repository extends BaseRepository
     use Signal, ErrorLog;
 
     /**
+     * @deprecated
+     *
      * @param       $uuid
-     * @param array $param
+     * @param array $arg
      *
      * @return mixed
      */
-    public function getIdFromUuid($uuid, array $param = [])
+    public function getIdFromUuid($uuid, array $arg = [])
+    {
+        $users = $this->getModel()::where('uuid', '=', $uuid)->orWhere('id', '=', $uuid)->get();
+
+        $uid = [];
+        $i   = 0;
+        foreach ($users as $user) {
+            $uid[$i] = $this->getModel()::where([
+                ['id', $user->id],
+                ['uuid', $uuid],
+            ])->first();
+
+            if (null === $uid[$i]) {
+                unset($uid[$i]);
+            } else {
+                return $uid[$i]->id;
+            }
+
+            $i++;
+        }
+    }
+
+    /**
+     * @param       $uuid
+     * @param array $arg
+     *
+     * @return mixed
+     */
+    public function getIdbyUuid($uuid, array $arg = [])
     {
         $users = $this->getModel()::where('uuid', '=', $uuid)->orWhere('id', '=', $uuid)->get();
 
