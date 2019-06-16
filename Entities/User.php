82899f34247172829e121bd8858d0beaab55da2a
anonymoussc <50c5ac69@opayq.com>
2019-06-17 00:09:15 +0700

    refactor(Entities): modified ./Entities/User

--- Entities/User.php
+++ Entities/User.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/22/19 8:34 AM
+ * User.php
+ * Created by @anonymoussc on 04/06/2019 12:32 PM.
  */
 
 /**
- * User.php
- * Created by @anonymoussc on 04/06/2019 12:32 PM.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/17/19 12:09 AM
  */
 
 namespace App\Components\Scaffold\Entities;
 
 use App\User as AppUser;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class User
@@ -27,7 +28,12 @@ class User extends AppUser
         parent::boot();
 
         static::creating(function($model) {
-            $model->uuid = (string)\Webpatser\Uuid\Uuid::generate(4);
+            $model->uuid       = (string)\Webpatser\Uuid\Uuid::generate(4);
+            $model->created_by = Auth::id() ?? 0;
+        });
+
+        static::updating(function($model) {
+            $model->updated_by = Auth::id() ?? 0;
         });
     }
 }
