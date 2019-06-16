90f12b2e997ea46cdbcc538370b9e6795c5a8cb2
anonymoussc <50c5ac69@opayq.com>
2019-06-16 18:19:29 +0700

    refactor(Services): modified ./Services/Role/Responses/RoleCollection

--- Services/Role/Responses/RoleCollection.php
+++ Services/Role/Responses/RoleCollection.php
@@ -1,15 +1,18 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/19/19 2:51 PM
- */
-
-/**
  * RoleCollection.php
  * Created by @anonymoussc on 05/10/2019 5:01 AM.
  */
 
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/16/19 5:52 PM
+ */
+
 namespace App\Components\Scaffold\Services\Role\Responses;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 
 /**
  * Class RoleCollection
@@ -17,6 +20,31 @@ namespace App\Components\Scaffold\Services\Role\Responses;
  */
 class RoleCollection
 {
+    /**
+     * @var \Illuminate\Auth\AuthManager|mixed
+     */
+    private $auth;
+
+    /**
+     * @var mixed
+     */
+    private $request;
+
+    /**
+     * @var mixed
+     */
+    private $appName;
+
+    /**
+     * UserCollection constructor.
+     */
+    public function __construct()
+    {
+        $this->auth    = App::get('auth');
+        $this->request = App::get('request');
+        $this->appName = Config::get('app.name') ?? Config::get('scaffold.name');
+    }
+
     /**
      * @param       $data
      * @param array $option
@@ -32,7 +60,7 @@ class RoleCollection
         if ($data->isNotEmpty()) {
             $newData = $data->map(function($value, $key) use ($param) {
                 return [
-                    'type'       => $param['type'],
+                    'type'       => Config::get('scaffold.api.roles.type'),
                     'id'         => $value->uuid,
                     'attributes' => [
                         'name'        => $value->name,
@@ -42,12 +70,12 @@ class RoleCollection
             });
 
             $records['data'] = $newData;
-            $records['link'] = $this->getLink($data, $param);
-            $records['meta'] = $this->getMeta($data, $param);
+            $records['link'] = $this->getLink($data);
+            $records['meta'] = $this->getMeta($data);
         } else {
             $records['data'] = [];
-            $records['link'] = $this->getLink($data, $param);
-            $records['meta'] = $this->getMeta($data, $param);
+            $records['link'] = $this->getLink($data);
+            $records['meta'] = $this->getMeta($data);
         }
 
         return $records;
@@ -71,7 +99,7 @@ class RoleCollection
                 'next'  => $data->nextPageUrl(),
             ];
         } else {
-            $link['self'] = $param['link.fullUrl'];
+            $link['self'] = $this->request->fullUrl();
         }
 
         return $link;
@@ -92,15 +120,15 @@ class RoleCollection
                 'current_page' => $data->currentPage(),
                 'from'         => $data->firstItem(),
                 'last_page'    => $data->lastPage(),
-                'path'         => $param['link.url'],
+                'path'         => $this->request->url(),
                 'per_page'     => $data->perPage(),
                 'to'           => $data->lastItem(),
                 'total'        => $data->total(),
             ];
         }
 
-        $meta['copyright'] = 'copyrightⒸ ' . date('Y') . ' ' . $param['app.name'];
-        $meta['author']    = $param['api.authors'];
+        $meta['copyright'] = 'copyrightⒸ ' . date('Y') . ' ' . $this->appName;
+        $meta['author']    = Config::get('scaffold.api.roles.authors');
 
         return $meta;
     }
