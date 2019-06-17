374eb6596db53fef0109738b841e2a37a7ed5472
anonymoussc <50c5ac69@opayq.com>
2019-06-17 15:38:07 +0700

    refactor(Services): modified ./Services/User/Requests/CreateUser

--- Services/User/Requests/CreateUser.php
+++ Services/User/Requests/CreateUser.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/22/19 1:26 AM
+ * CreateUser.php
+ * Created by @anonymoussc on 05/09/2019 7:20 PM.
  */
 
 /**
- * CreateUser.php
- * Created by @anonymoussc on 05/09/2019 7:20 PM.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/17/19 2:46 PM
  */
 
 namespace App\Components\Scaffold\Services\User\Requests;
@@ -28,7 +28,7 @@ class CreateUser
      */
     public function __invoke(array $data = [], array $option = [], array $param = [])
     {
-        $dataIn = $data['form'];
+        $dataIn = $data['input'];
         $user   = [];
         $inList = $data['inList'];
         $roleId = (int)$dataIn['roleId'];
@@ -39,7 +39,7 @@ class CreateUser
                 'uuid'              => (string)Uuid::generate(5, $dataIn['username'], Uuid::NS_DNS),
                 'username'          => strtolower($dataIn['username']),
                 'name'              => $dataIn['name'],
-                'email'             => strtolower($dataIn['email']),
+                'email'             => $dataIn['email'],
                 'avatar'            => $dataIn['avatar'],
                 'email_verified_at' => $dataIn['emailVerifiedAt'],
                 'password'          => password_hash($dataIn['password'], PASSWORD_BCRYPT),
