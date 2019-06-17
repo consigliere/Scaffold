0e9eeb8789da443192e0364fe3b50ed8156000dd
anonymoussc <50c5ac69@opayq.com>
2019-06-17 15:39:15 +0700

    refactor(Services): modified ./Services/PermissionService

--- Services/PermissionService.php
+++ Services/PermissionService.php
@@ -6,7 +6,7 @@
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 6/16/19 11:41 PM
+ * Last modified 6/17/19 3:35 PM
  */
 
 namespace App\Components\Scaffold\Services;
@@ -18,6 +18,7 @@ use App\Components\Scaffold\Services\Permission\Responses\PermissionCollection;
 use App\Components\Scaffold\Services\Permission\Responses\PermissionResource;
 use App\Components\Signature\Exceptions\BadRequestHttpException;
 use App\Components\Signature\Exceptions\NotFoundHttpException;
+use App\Components\Signature\Exceptions\UnprocessableEntityHttpException;
 use Illuminate\Foundation\Application;
 
 /**
@@ -78,10 +79,11 @@ class PermissionService extends Service
      */
     public function create(array $data, array $option = [], array $param = []): array
     {
-        $permissionKey = $this->permissionRepository->getWhere('key', data_get($data, 'form.key'));
+        $key           = data_get($data, 'input.key');
+        $permissionKey = $this->permissionRepository->getWhere('key', $key);
 
         if ($permissionKey->isNotEmpty()) {
-            throw new BadRequestHttpException('Permission name key already exists, please try another');
+            throw new BadRequestHttpException("Permission name key $key already exists, please try another");
         }
 
         $newPermission = (new CreatePermission)($data);
@@ -103,13 +105,13 @@ class PermissionService extends Service
         $id = $this->permissionRepository->getIdbyUuid($uuid);
 
         if (null === $id) {
-            throw new BadRequestHttpException('Can\'t find Permission with ID #' . $uuid);
+            throw new NotFoundHttpException('Cannot find Permissions resources in URI query parameter /' . $uuid);
         }
 
         $permission = $this->permissionRepository->getById($id);
 
         if (null === $permission) {
-            throw new NotFoundHttpException('Permission not found');
+            throw new BadRequestHttpException('Cannot find Permission with ID #' . $uuid);
         }
 
         return (new PermissionResource)($permission);
@@ -125,17 +127,22 @@ class PermissionService extends Service
      */
     public function update($uuid, array $data, array $option = [], array $param = []): array
     {
-        $id = $this->permissionRepository->getIdbyUuid($uuid);
+        $id  = $this->permissionRepository->getIdbyUuid($uuid);
+        $key = data_get($data, 'input.key');
 
         if (null === $id) {
-            throw new BadRequestHttpException('Can\'t find Permission with ID #' . $uuid);
+            throw new NotFoundHttpException('Cannot find Permissions resources in URI query parameter /' . $uuid);
         }
 
-        if (null !== data_get($data, 'form.key')) {
-            $roleName = $this->permissionRepository->getWhere('key', data_get($data, 'form.key'));
+        if (null !== data_get($data, 'input.key')) {
+            $permissions = $this->permissionRepository->getWhere('key', strtolower($key));
 
-            if ($roleName->isNotEmpty()) {
-                throw new BadRequestHttpException('Permission name key already exists, please try another');
+            if ($permissions->isNotEmpty()) {
+                foreach ($permissions as $permission) {
+                    if ($permission->id !== $id) {
+                        throw new UnprocessableEntityHttpException("Permission name key $key already exists, please try another");
+                    }
+                }
             }
         }
 
@@ -157,7 +164,7 @@ class PermissionService extends Service
             $id = $this->permissionRepository->getIdbyUuid($id);
 
             if (null === $id) {
-                throw new BadRequestHttpException('Can\'t find Permission with ID #' . $uuid);
+                throw new BadRequestHttpException('Cannot find Permission with ID #' . $uuid);
             }
 
             $this->permissionRepository->delete($id);
