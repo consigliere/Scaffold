fcca564a8d2439109c939d335e81cac70872206a
anonymoussc <50c5ac69@opayq.com>
2019-06-20 03:37:58 +0700

    refactor(Repositories): modified ./Repositories/UserRepositoryInterface

--- Repositories/UserRepositoryInterface.php
+++ Repositories/UserRepositoryInterface.php
@@ -1,22 +1,48 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/9/19 4:49 AM
+ * UserRepositoryInterface.php
+ * Created by @anonymoussc on 04/08/2019 11:53 PM.
  */
 
 /**
- * UserRepositoryInterface.php
- * Created by @anonymoussc on 04/08/2019 11:53 PM.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/20/19 3:37 AM
  */
 
 namespace App\Components\Scaffold\Repositories;
 
 
+/**
+ * Interface UserRepositoryInterface
+ * @package App\Components\Scaffold\Repositories
+ */
 interface UserRepositoryInterface
 {
+    /**
+     * @param array $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return mixed
+     */
     public function browse(array $data = [], array $option = [], array $param = []);
 
+    /**
+     * @param array $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return mixed
+     */
     public function create(array $data = [], array $option = [], array $param = []);
 
+    /**
+     * @param       $id
+     * @param array $data
+     * @param array $option
+     * @param array $param
+     *
+     * @return mixed
+     */
     public function update($id, array $data = [], array $option = [], array $param = []);
 }
