bccd9e35a63973192ea07948cfd5859c4070b391
anonymoussc <50c5ac69@opayq.com>
2019-06-28 06:16:17 +0700

    refactor(Services): modified ./Services/Permission/Requests/UpdatePermission

--- Services/Permission/Requests/UpdatePermission.php
+++ Services/Permission/Requests/UpdatePermission.php
@@ -6,7 +6,7 @@
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 6/17/19 2:46 PM
+ * Last modified 6/28/19 6:15 AM
  */
 
 namespace App\Components\Scaffold\Services\Permission\Requests;
@@ -15,7 +15,7 @@ namespace App\Components\Scaffold\Services\Permission\Requests;
  * Class UpdatePermission
  * @package App\Components\Scaffold\Services\Permission\Requests
  */
-class UpdatePermission
+final class UpdatePermission
 {
     /**
      * @param array $data
