42aab614dbc980c55badc9894e1fb8e59a99eb6b
anonymoussc <50c5ac69@opayq.com>
2019-06-28 06:15:40 +0700

    refactor(Services): modified ./Services/Permission/Requests/CreatePermission

--- Services/Permission/Requests/CreatePermission.php
+++ Services/Permission/Requests/CreatePermission.php
@@ -6,7 +6,7 @@
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 6/17/19 2:46 PM
+ * Last modified 6/28/19 6:15 AM
  */
 
 namespace App\Components\Scaffold\Services\Permission\Requests;
@@ -15,7 +15,7 @@ namespace App\Components\Scaffold\Services\Permission\Requests;
  * Class CreatePermission
  * @package App\Components\Scaffold\Services\Permission\Requests
  */
-class CreatePermission
+final class CreatePermission
 {
     /**
      * @param array $data
