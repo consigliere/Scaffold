a90c5815306e38e3aeb99af3c8572ad80f933657
anonymoussc <50c5ac69@opayq.com>
2019-06-28 02:43:44 +0700

    feat(Services): new ./Services/User/Responses/RelatedAdditionalRolesCollection

---
+++ Services/User/Responses/RelatedAdditionalRolesCollection.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * RelatedAdditionalRolesCollection.php
+ * Created by @anonymoussc on 06/27/2019 4:54 PM.
+ */
+
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/28/19 2:43 AM
+ */
+
+namespace App\Components\Scaffold\Services\User\Responses;
+
+use Illuminate\Support\Facades\App;
+
+/**
+ * Class RelatedAdditionalRolesCollection
+ * @package App\Components\Scaffold\Services\User\Responses
+ */
+final class RelatedAdditionalRolesCollection
+{
+    /**
+     * @var \Illuminate\Auth\AuthManager|mixed
+     */
+    private $auth;
+
+    /**
+     * @var mixed
+     */
+    private $request;
+
+    /**
+     * @var mixed
+     */
+    private $appName;
+
+    /**
+     * RoleCollection constructor.
+     */
+    public function __construct()
+    {
+        $this->auth    = App::get('auth');
+        $this->request = App::get('request');
+        $this->appName = config('app.name') ?? config('scaffold.name');
+    }
+
+    /**
+     * @param       $additional
+     * @param array $option
+     * @param array $param
+     *
+     * @return array
+     */
+    public function __invoke($additional, array $option = [], array $param = [])
+    {
+        $records = [];
+
+        if ($additional->isNotEmpty()) {
+            $newData = $additional->map(static function($value, $key) use ($param) {
+                return [
+                    'type' => config('scaffold.api.roles.type'),
+                    'id'   => $value->uuid,
+                ];
+            });
+
+            $records['data']['additional-roles'] = $newData;
+        } else {
+            $records['data']['additional-roles'] = [];
+        }
+
+        $records['links'] = $this->getLink();
+        $records['meta']  = $this->getMeta();
+
+        return $records;
+    }
+
+    /**
+     * @param       $data
+     * @param array $param
+     *
+     * @return array
+     */
+    private function getLink($data = null, array $param = []): array
+    {
+        $link['self'] = $this->request->fullUrl();
+
+        return $link;
+    }
+
+    /**
+     * @param       $data
+     * @param array $param
+     *
+     * @return array
+     */
+    private function getMeta($data = null, array $param = []): array
+    {
+        $meta = [];
+
+        $meta['copyright'] = 'copyrightⒸ ' . date('Y') . ' ' . $this->appName;
+        $meta['author']    = config('scaffold.api.roles.authors');
+
+        return $meta;
+    }
+}
