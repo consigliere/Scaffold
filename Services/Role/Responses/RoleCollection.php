fb4b78d714cff2ea7d5d8f14ea71ac25a722d28c
anonymoussc <50c5ac69@opayq.com>
2019-06-28 06:18:18 +0700

    refactor(Services): modified ./Services/Role/Responses/RoleCollection

--- Services/Role/Responses/RoleCollection.php
+++ Services/Role/Responses/RoleCollection.php
@@ -6,7 +6,7 @@
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 6/28/19 3:47 AM
+ * Last modified 6/28/19 6:15 AM
  */
 
 namespace App\Components\Scaffold\Services\Role\Responses;
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Config;
  * Class RoleCollection
  * @package App\Components\Scaffold\Services\Role\Responses
  */
-class RoleCollection
+final class RoleCollection
 {
     /**
      * @var \Illuminate\Auth\AuthManager|mixed
