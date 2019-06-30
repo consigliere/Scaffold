b4926d550a48618f91a45cdd830396265d690678
anonymoussc <50c5ac69@opayq.com>
2019-07-01 02:32:30 +0700

    feat(Entities): modified ./Entities/Role

--- Entities/Role.php
+++ Entities/Role.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/12/19 7:22 AM
+ * Role.php
+ * Created by @anonymoussc on 04/09/2019 12:58 PM.
  */
 
 /**
- * Role.php
- * Created by @anonymoussc on 04/09/2019 12:58 PM.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 7/1/19 2:23 AM
  */
 
 namespace App\Components\Scaffold\Entities;
@@ -17,8 +17,14 @@ use TCG\Voyager\Models\Role as BaseRole;
 /**
  * Class Role
  * @package App\Components\Scaffold\Entities
+ * @property mixed $permissions
  */
 class Role extends BaseRole
 {
     use UuidsTrait;
+
+    public function permissions(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany('App\Components\Scaffold\Entities\Permission', 'permission_role', 'role_id', 'permission_id');
+    }
 }
