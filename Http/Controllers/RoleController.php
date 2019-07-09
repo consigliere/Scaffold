443de200ea650b28c1fd2b5c582f9722bea576fb
anonymoussc <50c5ac69@opayq.com>
2019-07-10 04:03:43 +0700

    refactor(Controllers): modified ./Http/Controllers/RoleController

--- Http/Controllers/RoleController.php
+++ Http/Controllers/RoleController.php
@@ -6,7 +6,7 @@
 
 /**
  * Copyright(c) 2019. All rights reserved.
- * Last modified 7/8/19 11:52 PM
+ * Last modified 7/10/19 2:36 AM
  */
 
 namespace App\Components\Scaffold\Http\Controllers;
@@ -48,7 +48,7 @@ class RoleController extends Controller
     {
         $data = [
             'header' => [
-                'paging' => $request->header('Page-Paging') ?? config('scaffold.api.page_paging'),
+                'paging' => $request->header('X-Page-Paging') ?? config('scaffold.api.page_paging'),
             ],
         ];
 
