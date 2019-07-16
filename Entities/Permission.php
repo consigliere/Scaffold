ff3183db9603734bff64ca32027b0ee6524354de
anonymoussc <50c5ac69@opayq.com>
2019-07-16 08:15:31 +0700

    refactor(Entities): modified ./Entities/Permission

--- Entities/Permission.php
+++ Entities/Permission.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/12/19 9:37 AM
+ * Permission.php
+ * Created by @anonymoussc on 04/09/2019 12:58 PM.
  */
 
 /**
- * Permission.php
- * Created by @anonymoussc on 04/09/2019 12:58 PM.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 7/16/19 7:25 AM
  */
 
 namespace App\Components\Scaffold\Entities;
@@ -17,4 +17,18 @@ use TCG\Voyager\Models\Permission as AppPermission;
 class Permission extends AppPermission
 {
     use UuidsTrait;
+
+    public static function generateFor($table_name)
+    {
+        self::firstOrCreate(['uuid' => randomUuid(), 'key' => 'browse_' . $table_name, 'table_name' => $table_name]);
+        self::firstOrCreate(['uuid' => randomUuid(), 'key' => 'read_' . $table_name, 'table_name' => $table_name]);
+        self::firstOrCreate(['uuid' => randomUuid(), 'key' => 'edit_' . $table_name, 'table_name' => $table_name]);
+        self::firstOrCreate(['uuid' => randomUuid(), 'key' => 'add_' . $table_name, 'table_name' => $table_name]);
+        self::firstOrCreate(['uuid' => randomUuid(), 'key' => 'delete_' . $table_name, 'table_name' => $table_name]);
+    }
+
+    public static function removeFrom($table_name)
+    {
+        self::where(['table_name' => $table_name])->delete();
+    }
 }
