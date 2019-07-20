4429eacb6bd82cd8d40f2c89a1dd541a0211a4b6
anonymoussc <50c5ac69@opayq.com>
2019-07-21 02:59:10 +0700

    feat(Tests): modified ./Tests/Feature/CreateUsersTest

---
+++ Tests/Feature/CreateUsersTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * CreateUsersTest.php
+ * Created by @anonymoussc on 07/20/2019 10:40 PM.
+ */
+
+/**
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 7/21/19 2:59 AM
+ */
+
+namespace App\Components\Scaffold\Tests\Feature;
+
+/**
+ * Class CreateUsersTest
+ * @package App\Components\Scaffold\Tests\Feature
+ */
+class CreateUsersTest extends \Tests\ApiTestCase
+{
+    /**
+     * @return void
+     */
+    public function testCreateUsers(): void
+    {
+        $this->init();
+
+        $header = [
+            'Accept'       => 'application/vnd.api+json',
+            'Content-Type' => 'application/vnd.api+json',
+        ];
+
+        $response = $this->json('POST', '/api/v1/users', [
+            "roleId"          => "",
+            "username"        => "dummytests" . mt_rand(),
+            "name"            => "Dummy" . mt_rand() . " Tests",
+            "email"           => "dummytests" . mt_rand() . "@email.com",
+            "avatar"          => "",
+            "emailVerifiedAt" => "",
+            "password"        => "dummytests" . mt_rand(),
+            "rememberToken"   => "",
+            "settings"        => "",
+        ], $header);
+
+        // $response->dumpHeaders();
+
+        // $response->dump();
+
+        $response->assertStatus(201);
+    }
+}
